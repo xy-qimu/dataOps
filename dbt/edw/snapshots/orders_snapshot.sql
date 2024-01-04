74405a011d0c9a95d2800c7175f44cc9c627188f
qimu <hualin_xie@163.com>
2024-01-04 16:52:16 +0800

    添加 snapshot

---
+++ dbt/edw/snapshots/orders_snapshot.sql
@@ -0,0 +1,14 @@
+{% snapshot orders_snapshot %}
+
+{{
+    config(
+      target_schema='snapshots',
+      unique_key='id',
+      strategy='timestamp',
+      updated_at='order_date',
+    )
+}}
+
+select * from {{ ref('raw_orders') }}
+
+{% endsnapshot %}
