e87478eb4924d3440a83a17e0d8faecb8a702661
qimu <hualin_xie@163.com>
2024-07-14 12:37:12 +0800

    1

---
+++ install_odbc.sh
@@ -0,0 +1,10 @@
+curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc
+
+# Debian 11
+curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
+
+# optional: for unixODBC development headers
+sudo apt-get install -y unixodbc-dev
